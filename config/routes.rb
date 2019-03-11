85d2cf9e31fb1af191290ce1fb4f7aa2d4718bf9
letavi <ltviet.nk@gmail.com>
2019-03-11 22:33:55 +0700

    Custom path for sign_in, sign_up, log_out

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  as :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'signup', to: 'devise/registrations#new'
+    delete 'logout', to: 'devise/sessions#destroy'
+  end
   root 'pages#home'
 end
