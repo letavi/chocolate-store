8d765c3d0080a55a930ce168203c432bb01d8398
letavi <ltviet.nk@gmail.com>
2019-03-12 09:57:09 +0700

    Add username field to users table.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   as :user do
     get 'login', to: 'devise/sessions#new'
     get 'signup', to: 'devise/registrations#new'
